6ccf66287214ece88184a503c6ba3084c4cac679
Damian Eduard Costel <eduarddamian21@gmail.com>
2025-11-01 19:46:21 +0200

    Created initial database logic

--- TheGame/Database/Database/DatabaseManager.h
+++ TheGame/Database/Database/DatabaseManager.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <string>
+#include <vector>
+
+struct User {
+    int id;
+    std::string username;
+    std::string password;
+};
+
+struct Game {
+    int id;
+    std::string player;
+    int score;
+    std::string date;
+};
+
+class DatabaseManager {
+private:
+    std::string databasePath;
+
+public:
+    DatabaseManager(const std::string& dbPath);
+
+    void addUser(const std::string& username, const std::string& password);
+    bool userExists(const std::string& username);
+
+    void addGameResult(const std::string& player, int score);
+    std::vector<Game> getGamesByUser(const std::string& username);
+};
+
