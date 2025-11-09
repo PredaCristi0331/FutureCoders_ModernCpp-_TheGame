6dc43ab717e9f27d9ebb1c9bbbd53ea260bfc09c
stantimotei123 <stan.timotei123@gmail.com>
2025-11-09 18:59:44 +0200

    i created the lobbywindow with welcome mesage,select difficulty

---
+++ TheGame/ClientConsoleQt/LobbyWindow.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <QWidget>
+#include <QString>
+#include <QComboBox>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTimer>
+
+/**
+ * LobbyWindow - Fereastra de lobby cu selecție dificultate
+ * 
+ * Permite utilizatorului să:
+ * - Selecteze nivelul de dificultate (Ușor/Mediu/Greu)
+ * - Caută un joc sau pornește un joc nou
+ * - Vezi statusul de matchmaking
+ */
+class LobbyWindow : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit LobbyWindow(QWidget* parent = nullptr);
+    ~LobbyWindow();
+
+    void setUsername(const QString& username);
+
+signals:
+    void startGame(const QString& username, const QString& difficulty);
+    void showProfile(const QString& username);
+    void logout();
+
+private slots:
+    void onPlayClicked();
+    void onProfileClicked();
+    void onLogoutClicked();
+    void onDifficultyChanged(int index);
+    void onMatchmakingTimeout();
+
+private:
+    QString currentUsername;
+    QString selectedDifficulty;
+
+    // Widget-uri UI
+    QLabel* welcomeLabel;
+    QLabel* difficultyLabel;
+    QComboBox* difficultyComboBox;
+    QPushButton* playButton;
+    QPushButton* profileButton;
+    QPushButton* logoutButton;
+    QLabel* statusLabel;
+    QTimer* matchmakingTimer;
+
+    void setupUI();
+    void applyStyles();
+    void connectSignals();
+    void updateStatus(const QString& message, const QString& color);
+};
+
