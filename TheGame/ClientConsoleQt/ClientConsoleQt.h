09ecbaf25fb02b0de0edca53420af384ea26afcf
stantimotei123 <stan.timotei123@gmail.com>
2025-11-09 18:48:38 +0200

    i integrated login window in clientconsoleqt and updated the main(COSMIN)

--- TheGame/ClientConsoleQt/ClientConsoleQt.h
+++ TheGame/ClientConsoleQt/ClientConsoleQt.h
@@ -2,6 +2,7 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_ClientConsoleQt.h"
+#include "LoginWindow.h"
 
 class ClientConsoleQt : public QMainWindow
 {
@@ -11,7 +12,15 @@ public:
     ClientConsoleQt(QWidget *parent = nullptr);
     ~ClientConsoleQt();
 
+private slots:
+    void onLoginSuccessful(const QString& username);
+
 private:
     Ui::ClientConsoleQtClass ui;
+    LoginWindow* loginWindow;
+    QString currentUsername;
+    
+    void showLoginWindow();
+    void showMainWindow();
 };
 
