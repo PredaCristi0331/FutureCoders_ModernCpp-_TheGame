2eea29bb2ecc6bc417c0d0aeb61b52253da86623
Damian Eduard Costel <eduarddamian21@gmail.com>
2025-11-22 18:27:13 +0200

    statistics data models

--- TheGame/Database/Database/Statistics.h
+++ TheGame/Database/Database/Statistics.h
@@ -1,1 +1,39 @@
 #pragma once
+
+#include <string>
+
+struct User {
+    int id;
+    std::string username;
+    std::string passwordHash;
+    std::string createdAt;
+};
+
+struct GameSession {
+    int id;
+    std::string startTime;
+    std::string endTime;
+    int numPlayers;
+    int customLevel;
+    int teamWon;
+    int cardsLeftDeck;
+};
+
+struct PlayerStatistics {
+    int id;
+    int userId;
+    int gameSessionId;
+    int cardsInHandEnd;
+	int performanceScore;
+    int wasHost;
+};
+
+struct UserProfileStats {
+    int userId;
+    std::string username;
+    int totalGamesPlayed;
+    int totalWins;
+    double winRate;
+	double avgCardsLostGames;
+	double averagePerformance;
+};
