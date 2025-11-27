5b5bdd0db941c8686e51c03ea0eec08f5ae83d90
stantimotei123 <stan.timotei123@gmail.com>
2025-11-27 10:10:58 +0200

    updated card.cpp

--- TheGame/TheGame/Card.cpp
+++ TheGame/TheGame/Card.cpp
@@ -1,21 +1,39 @@
-#include "Card.h"
+export module Card;
 
-Card::Card(int card) : m_Card(card) {
-	if (card == 1 || card == 100)
-		this->m_typeCard = TypeCard::FUNDCARD;
-	else this->m_typeCard = TypeCard::GAMECARD;
-}
+import <iostream>;
+import <string>;
 
-int Card::GetCardNumber() const { return m_Card; }
+namespace game {
+	export class Card
+	{
+		enum class TypeCard
+		{
+			GAMECARD,
+			FUNDCARD
+		};
+	public:
+		Card(int card) : m_Card(card) {
+			if (card == 1 || card == 100)
+				this->m_typeCard = TypeCard::FUNDCARD;
+			else this->m_typeCard = TypeCard::GAMECARD;
+		}
 
-std::string Card::GetTypeCard() const {
-	if (m_typeCard == TypeCard::GAMECARD)
-		return "GAMECARD";
-	else return "FUNDCARD";
-}
+		int GetCardNumber() const { return m_Card; }
+		
+		std::string GetTypeCard() const {
+			if (m_typeCard == TypeCard::GAMECARD)
+				return "GAMECARD";
+			else return "FUNDCARD";
+		}
 
-bool Card::operator==(const Card& card) const {
-	if (this->GetCardNumber() == card.GetCardNumber())
-		return true;
-	return false;
+		bool operator==(const Card& card) const {
+			if (this->GetCardNumber() == card.GetCardNumber())
+				return true;
+			return false;
+		}
+
+	private:
+		int m_Card;
+		TypeCard m_typeCard;
+	};
 }
