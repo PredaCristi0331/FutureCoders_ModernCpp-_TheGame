1920f81b942c8380554463abf3c5b410d3af216d
stantimotei123 <stan.timotei123@gmail.com>
2025-11-27 10:14:38 +0200

    add module implementations as .cpp files for Jucator

--- TheGame/TheGame/Jucator.cpp
+++ TheGame/TheGame/Jucator.cpp
@@ -1,39 +1,52 @@
-//#include "Jucator.h"
-module Jucator;
-using namespace game;
-import std;
+export module Jucator;
+export import Card;
 
-Jucator::Jucator()
-{
-}
+import <vector>;
+import <iostream>;
+import <algorithm>;
 
-void Jucator::AddCard(Card cardDrawn)
-{
-	this->m_cards.push_back(cardDrawn);
-}
-
-std::vector<Card> Jucator::GetCards()
-{
-	return this->m_cards;
-}
-
-void Jucator::ShowCards()
-{
-	for (int i = 0; i < m_cards.size(); i++) {
-		std::cout << m_cards[i].GetCardNumber() << " ";
-	}
-}
-
-Card Jucator::CardLaidDown(int card)
-{
-	int j = -1;
-	for(int i=0;i<m_cards.size();i++)
-		if (m_cards[i].GetCardNumber() == card) {
-			j = i;
-			break;
+namespace game {
+	export class Jucator
+	{
+	private:
+		std::vector<Card>m_cards;
+	public:
+		Jucator() {}
+		
+		void AddCard(Card cardDrawn) {
+			this->m_cards.push_back(cardDrawn);
 		}
-	if (j > -1)
-		for (int i = j; i < m_cards.size() - 1; i++)
-			m_cards[i] = m_cards[i + 1];
-	return m_cards[j];
+
+		std::vector<Card> GetCards() const {
+			return this->m_cards;
+		}
+
+		void ShowCards() {
+			for (int i = 0; i < m_cards.size(); i++) {
+				std::cout << m_cards[i].GetCardNumber() << " ";
+			}
+		}
+
+		Card CardLaidDown(int card) {
+			int j = -1;
+			for(int i=0;i<m_cards.size();i++)
+				if (m_cards[i].GetCardNumber() == card) {
+					j = i;
+					break;
+				}
+			if (j > -1)
+				for (int i = j; i < m_cards.size() - 1; i++)
+					m_cards[i] = m_cards[i + 1];
+			return m_cards[j];
+		}
+
+		void RemoveCard(Card card) {
+			auto it = std::remove_if(m_cards.begin(), m_cards.end(), [&](const Card& c) {
+				return c.GetCardNumber() == card.GetCardNumber();
+			});
+			if (it != m_cards.end()) {
+				m_cards.erase(it, m_cards.end());
+			}
+		}
+	};
 }
