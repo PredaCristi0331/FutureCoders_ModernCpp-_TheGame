7d6524f4313f90112d67931bf58264c82e8c72c4
PredaCristi0331 <97984945+PredaCristi0331@users.noreply.github.com>
2025-12-11 15:27:17 +0200

    Adding main in the TestsProject solution so as to run all the tests

--- TheGame/TestsProject/test_main.cpp
+++ TheGame/TestsProject/test_main.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+#include "TestRunner.h"
+
+// include all headers so tests auto-register
+#include "TestUtils.h"
+#include "TestGameLogic.h"
+#include "TestInventory.h"
+#include "TestPlayer.h"
 
 int main() {
-	return 0;
+    // Run everything that registered via REGISTER_TEST
+    TestRegistry::instance().runAll();
+    return 0;
 }
