f636a640b0875f4239d98699c90fa97519f16b27
Damian Eduard Costel <eduarddamian21@gmail.com>
2025-12-13 17:23:49 +0200

    refined models

--- TheGame/Database/Database/db/Models.h
+++ TheGame/Database/Database/db/Models.h
@@ -1,41 +1,71 @@
 #pragma once
 #include <string>
+#include <cstdint>
 
 struct User {
     int id;                     
     std::string username;      
     std::string password;
+
+    std::int64_t hours_played_seconds = 0;
+    int performance_score = 1;
+};
+
+enum class GameStatus : int {
+    Waiting = 0,
+    Running = 1,
+    Finished = 2
 };
 
 struct GameSession {
-    int id;
+    int id = 0;
+
+    std::string created_at;
     std::string start_time;
     std::string end_time;
-    std::string difficulty;
-    int num_players;
-    bool won;
-    int cards_left_in_draw;
-    int total_moves;
+
+    GameStatus status = GameStatus::Waiting;
+
+    int num_players = 0;
+    bool won = false;
+
+    int cards_left_in_draw = 0;
+    int total_moves = 0;
+
+    std::int64_t duration_seconds = 0;
 };
 
 struct PlayerGameStats {
     int id;
     int user_id;
     int game_session_id;
-    bool is_host;
-    int final_cards_in_hand;
-    int moves_played;
-    bool won;
+
+    bool is_host = false;
+
+    int final_cards_in_hand = 0;
+    int moves_played = 0;
+
+    bool won = false;
+};
+
+struct ChatMessage {
+    int id = 0;
+    int game_session_id = 0;
+    int user_id = 0;
+
+    std::string message;
+    std::string timestamp;
 };
 
 struct UserProfile {
     std::string username;
 
-    int games_played;
-    int games_won;
-    int games_lost;
+    int games_played = 0;
+    int games_won = 0;
+    int games_lost = 0;
 
-    double avg_cards_on_loss;
+    double avg_cards_on_loss = 0.0;
 
-    int performance_score;
+    int performance_score = 1;
+    std::int64_t hours_played_seconds = 0;
 };
